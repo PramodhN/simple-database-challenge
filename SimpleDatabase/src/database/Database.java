bf288d3f0d5030b34ae7c76725eb5ee11b3c2888
pramodhn <pnatara7@asu.edu>
2016-03-16 22:04:05 -0700

    base code for reading inputs

--- SimpleDatabase/src/database/Database.java
+++ SimpleDatabase/src/database/Database.java
@@ -1,16 +1,78 @@
 package database;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import database.operations.DatabaseOperations;
 
 public class Database {
 	private static DatabaseOperations db;
 
-	private static void fileInput(String string) {
+	private final static String BEGIN = "BEGIN";
+	private final static String SET = "SET";
+	private final static String GET = "GET";
+	private final static String UNSET = "UNSET";
+	private final static String NUMEQUALTO = "NUMEQUALTO";
+	private final static String ROLLBACK = "ROLLBACK";
+	private final static String COMMIT = "COMMIT";
+	private final static String END = "END";
 
+	private static void handleInputLine(String line) {
+		String[] parameters = line.split("\\s");
+		String operation = parameters[0];
+		switch (operation) {
+		case BEGIN:
+			db.begin();
+			break;
+		case GET:
+			break;
+		case SET:
+			break;
+		case UNSET:
+			break;
+		case NUMEQUALTO:
+			break;
+		case ROLLBACK:
+			break;
+		case COMMIT:
+			break;
+		default:
+			System.err.println("Invalid operation: " + operation + "\nIgnoring this line.");
+			break;
+		}
+	}
+
+	private static void fileInput(String inputFileName) {
+		try {
+			File file = new File(inputFileName);
+			FileReader fileReader = new FileReader(file);
+			BufferedReader reader = new BufferedReader(fileReader);
+			String line;
+			while ((line = reader.readLine()) != null) {
+				handleInputLine(line);
+			}
+			fileReader.close();
+		} catch (IOException e) {
+			System.err.println("Issue with reading the file.");
+			e.printStackTrace();
+		}
 	}
 
 	private static void commandLineInput() {
-
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String line = reader.readLine();
+			while (!line.equals(END)) {
+				handleInputLine(line);
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			System.err.print("Issue with command line input");
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
--- SimpleDatabase/src/database/operations/DatabaseOperations.java
+++ SimpleDatabase/src/database/operations/DatabaseOperations.java
@@ -1,5 +1,14 @@
 package database.operations;
 
+import java.util.HashMap;
+
 public class DatabaseOperations {
+	HashMap<String, Integer> variables;
+	HashMap<Integer, Integer> valueCounts;
+
+	public void begin() {
+		variables = new HashMap<String, Integer>();
+		valueCounts = new HashMap<Integer, Integer>();
+	}
 
 }
